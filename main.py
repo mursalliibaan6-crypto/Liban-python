81facf3b62e93717167d6f221bc14eaaaaecdf57
mursalliibaan6-crypto <mursalliibaan6@gmail.com>
2026-06-29 13:27:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+
+my_time = int(input("enter the time: "))
+
+for x in range(0, my_time):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+    
+print("time's up")    
+    
+    
